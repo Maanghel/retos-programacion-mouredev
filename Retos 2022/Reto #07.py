51e761099f94094a3c178e8dd0168117a3ae5ebd
Manuel Gamez <138623553+Maanghel@users.noreply.github.com>
2025-06-03 02:27:32 -0700

    feat: agrega la solucion al problema 07

---
+++ Retos 2022/Reto #07.py
@@ -0,0 +1,26 @@
+"""
+Crea un programa que invierta el orden de una cadena de texto
+    sin usar funciones propias del lenguaje que lo hagan de forma automática.
+- Si le pasamos "Hola mundo" nos retornaría "odnum aloH"
+"""
+
+def reverse_text(text: str) -> str:
+    """
+    Reverses the given text.
+
+    This function takes a string as input and returns a new string
+    with the characters in reverse order.
+
+    Parameters:
+    - text : str
+        The input string to be reversed.
+
+    Returns:
+    - str: The reversed string.
+    """
+    reverse = ""
+    for char in text:
+        reverse = char + reverse
+    return reverse
+
+print(reverse_text("Hola mundo"))
