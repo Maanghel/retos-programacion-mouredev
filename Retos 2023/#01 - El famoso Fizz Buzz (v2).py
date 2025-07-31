8ed03637d4829406ab278e0b3df8f548b5da6b81
Maanghel <138623553+Maanghel@users.noreply.github.com>
2025-07-31 14:08:50 -0700

    Pequeña modificacion de lineas

--- Retos 2023/#01 - El famoso Fizz Buzz (v2).py
+++ Retos 2023/#01 - El famoso Fizz Buzz (v2).py
@@ -18,11 +18,11 @@ def fizz_buzz() -> None:
     for i in range(1, 101):
         if i % 3 == 0 and i % 5 == 0: 
             print("fizzbuzz")
-        elif i % 3 == 0: 
+        elif i % 3 == 0:
             print("fizz")
-        elif i % 5 == 0: 
+        elif i % 5 == 0:
             print("buzz")
-        else: 
+        else:
             print(i)
 
 
